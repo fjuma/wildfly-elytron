4a98ca5f1f603e32c4aca99a6069426f7eef1dcb
Farah Juma <fjuma@redhat.com>
2016-03-29 15:27:38 -0400

    [ELY-427] Add the ability to make use of a pre-established SecurityIdentity to the CLIENT_CERT HTTP authentication mechanism

--- src/main/java/org/wildfly/security/http/impl/ClientCertAuthenticationMechanism.java
+++ src/main/java/org/wildfly/security/http/impl/ClientCertAuthenticationMechanism.java
@@ -30,12 +30,14 @@ import javax.net.ssl.SSLSession;
 import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.UnsupportedCallbackException;
+import javax.security.sasl.AuthorizeCallback;
 
 import org.wildfly.security._private.ElytronMessages;
 import org.wildfly.security.auth.callback.AuthenticationCompleteCallback;
 import org.wildfly.security.auth.callback.EvidenceVerifyCallback;
 import org.wildfly.security.auth.callback.SecurityIdentityCallback;
 import org.wildfly.security.auth.server.SecurityIdentity;
+import org.wildfly.security.evidence.SecurityIdentityEvidence;
 import org.wildfly.security.evidence.X509PeerCertificateChainEvidence;
 import org.wildfly.security.http.HttpAuthenticationException;
 import org.wildfly.security.http.HttpServerAuthenticationMechanism;
@@ -67,43 +69,53 @@ public class ClientCertAuthenticationMechanism implements HttpServerAuthenticati
             return;
         }
 
+        boolean authenticated = false;
         SecurityIdentity securityIdentity = (SecurityIdentity) sslSession.getValue(SSL_SESSION_IDENTITY_KEY);
         if (securityIdentity != null) {
-            // TODO We need to check this is applicable for our domain and use a 'converted' value.
-            request.authenticationComplete(securityIdentity);
-            return;
-        }
-
-        final X509Certificate[] peerX509Certificates;
-        try {
-            Certificate[] peerCertificates = sslSession.getPeerCertificates();
-            peerX509Certificates = new X509Certificate[peerCertificates.length];
-            for (int i=0;i<peerCertificates.length; i++) {
-                if (peerCertificates[i] instanceof X509Certificate) {
-                    peerX509Certificates[i] = (X509Certificate) peerCertificates[i];
-                } else {
-                    request.noAuthenticationInProgress();
-                    return;
+            final SecurityIdentityEvidence evidence = new SecurityIdentityEvidence(securityIdentity);
+            final EvidenceVerifyCallback evidenceVerifyCallback = new EvidenceVerifyCallback(evidence);
+            try {
+                callbackHandler.handle(new Callback[] { evidenceVerifyCallback });
+                if (evidenceVerifyCallback.isVerified()) {
+                    final AuthorizeCallback authorizeCallback = new AuthorizeCallback(null, null);
+                    callbackHandler.handle(new Callback[] { authorizeCallback });
+                    if (authorizeCallback.isAuthorized()) {
+                        authenticated = true;
+                    }
                 }
+            } catch (IOException e) {
+                throw new HttpAuthenticationException(e);
+            } catch (UnsupportedCallbackException e) {
             }
-        } catch (SSLPeerUnverifiedException e) {
-            ElytronMessages.log.trace("Peer not verified.");
-            request.noAuthenticationInProgress();
-            return;
+        } else {
+            final X509Certificate[] peerX509Certificates;
+            try {
+                Certificate[] peerCertificates = sslSession.getPeerCertificates();
+                peerX509Certificates = new X509Certificate[peerCertificates.length];
+                for (int i = 0; i < peerCertificates.length; i++) {
+                    if (peerCertificates[i] instanceof X509Certificate) {
+                        peerX509Certificates[i] = (X509Certificate) peerCertificates[i];
+                    } else {
+                        request.noAuthenticationInProgress();
+                        return;
+                    }
+                }
+            } catch (SSLPeerUnverifiedException e) {
+                ElytronMessages.log.trace("Peer not verified.");
+                request.noAuthenticationInProgress();
+                return;
+            }
+
+            final X509PeerCertificateChainEvidence evidence = new X509PeerCertificateChainEvidence(peerX509Certificates);
+            EvidenceVerifyCallback evc = new EvidenceVerifyCallback(evidence);
+
+            try {
+                callbackHandler.handle(new Callback[] { evc });
+                authenticated = evc.isVerified();
+            } catch (IOException e) {
+                throw new HttpAuthenticationException(e);
+            } catch (UnsupportedCallbackException e) {}
         }
-
-        final X509PeerCertificateChainEvidence evidence = new X509PeerCertificateChainEvidence(peerX509Certificates);
-
-        EvidenceVerifyCallback evc = new EvidenceVerifyCallback(evidence);
-        boolean authenticated = false;
-
-        try {
-            callbackHandler.handle(new Callback[] { evc });
-            authenticated = evc.isVerified();
-        } catch (IOException e) {
-            throw new HttpAuthenticationException(e);
-        } catch (UnsupportedCallbackException e) {}
-
         try {
             if (authenticated) {
                 SecurityIdentityCallback securityIdentityCallback = new SecurityIdentityCallback();
