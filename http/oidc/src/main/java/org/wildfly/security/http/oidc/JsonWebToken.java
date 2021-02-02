85d6062ce01dd92ae9523e0b24c733428b9be1ed
Farah Juma <fjuma@redhat.com>
2021-02-02 17:44:00 -0500

    wip

--- http/oidc/src/main/java/org/wildfly/security/http/oidc/JsonWebToken.java
+++ http/oidc/src/main/java/org/wildfly/security/http/oidc/JsonWebToken.java
@@ -18,192 +18,90 @@
 
 package org.wildfly.security.http.oidc;
 
-import java.io.Serializable;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
-import org.wildfly.security.json.util.StringOrArrayDeserializer;
-import org.wildfly.security.json.util.StringOrArraySerializer;
-
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.jose4j.jwt.JwtClaims;
+import org.jose4j.jwt.MalformedClaimException;
 
 /**
  * Representation of a JSON Web Token, as per <a href="https://tools.ietf.org/html/rfc7519">RFC 7519</a>.
  *
- * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
  * @author <a href="mailto:fjuma@redhat.com">Farah Juma</a>
  */
-public class JsonWebToken implements Serializable, Token {
+public class JsonWebToken {
 
-    public static final String ISS = "iss";
-    public static final String SUB = "sub";
-    public static final String AUD = "aud";
     public static final String EXP = "expiration";
     public static final String NBF = "notBefore";
     public static final String IAT = "iat";
-    public static final String JTI = "jti";
-    public static final String TYP = "typ";
 
-    @JsonProperty(ISS)
-    protected String issuer;
 
-    @JsonProperty(SUB)
-    protected String subject;
+    private final JwtClaims jwtClaims;
 
-    @JsonProperty(AUD)
-    @JsonSerialize(using = StringOrArraySerializer.class)
-    @JsonDeserialize(using = StringOrArrayDeserializer.class)
-    protected String[] audience;
-
-    @JsonProperty(EXP)
-    protected Long expiration;
-
-    @JsonProperty(NBF)
-    protected Long notBefore;
-
-    @JsonProperty(IAT)
-    protected Long iat;
-
-    @JsonProperty(JTI)
-    protected String id;
-
-    @JsonProperty(TYP)
-    protected String type;
-
-    protected Map<String, Object> otherClaims = new HashMap<>();
-
-    public String getIssuer() {
-        return issuer;
+    public JsonWebToken(JwtClaims jwtClaims) {
+        this.jwtClaims = jwtClaims;
     }
 
-    public JsonWebToken setIssuer(String issuer) {
-        this.issuer = issuer;
-        return this;
+    public String getIssuer() {
+        try {
+            return jwtClaims.getIssuer();
+        } catch (MalformedClaimException e) {
+
+        }
     }
 
     public String getSubject() {
-        return subject;
-    }
+        try {
+            return jwtClaims.getSubject();
+        } catch (MalformedClaimException e) {
 
-    public JsonWebToken setSubject(String subject) {
-        this.subject = subject;
-        return this;
-    }
-
-    @JsonIgnore
-    public String[] getAudience() {
-        return audience;
-    }
-
-    public boolean hasAudience(String audience) {
-        if (this.audience == null) {
-            return false;
         }
-        for (String a : this.audience) {
-            if (a.equals(audience)) {
-                return true;
-            }
-        }
-        return false;
     }
 
-    public JsonWebToken setAudience(String... audience) {
-        this.audience = audience;
-        return this;
-    }
+    public List<String> getAudience() {
+        try {
+            return jwtClaims.getAudience();
+        } catch (MalformedClaimException e) {
 
-    public JsonWebToken addAudience(String audience) {
-        if (this.audience == null) {
-            this.audience = new String[] { audience };
-        } else {
-            // Check if audience is already there
-            for (String aud : this.audience) {
-                if (audience.equals(aud)) {
-                    return this;
-                }
-            }
-
-            String[] newAudience = Arrays.copyOf(this.audience, this.audience.length + 1);
-            newAudience[this.audience.length] = audience;
-            this.audience = newAudience;
         }
-        return this;
     }
 
     public Long getExpiration() {
-        return expiration;
+        return getClaimValueAsLong(EXP);
     }
 
-    @JsonIgnore
     public boolean isExpired() {
+        Long expiration = getExpiration();
         return expiration != null && expiration != 0 ? getCurrentTimeInSeconds() > expiration : false;
     }
 
-    public JsonWebToken setExpiration(Long expiration) {
-        this.expiration = expiration;
-        return this;
-    }
-
     public Long getNotBefore() {
-        return notBefore;
+        return getClaimValueAsLong(NBF);
     }
 
-    @JsonIgnore
     public boolean isNotBefore() {
+        Long notBefore = getNotBefore();
         return notBefore != null ? getCurrentTimeInSeconds() >= notBefore : true;
     }
 
-    public JsonWebToken setNotBefore(Long notBefore) {
-        this.notBefore = notBefore;
-        return this;
-    }
     /**
      * Checks that the token is not expired and isn't prior to not-before.
      *
      * @return {@code true} if the token is active; {@code false} otherwise
      */
-    @JsonIgnore
     public boolean isActive() {
         return !isExpired() && isNotBefore();
     }
 
     public Long getIssuedAt() {
-        return iat;
-    }
-
-    @JsonIgnore
-    public JsonWebToken setIssuedNow() {
-        iat = Long.valueOf(getCurrentTimeInSeconds());
-        return this;
-    }
-
-    public JsonWebToken setIssuedAt(Long iat) {
-        this.iat = iat;
-        return this;
+        return getClaimValueAsLong(IAT);
     }
 
     public String getId() {
-        return id;
-    }
+        try {
+            return jwtClaims.getJwtId();
+        } catch (MalformedClaimException e) {
 
-    public JsonWebToken setId(String id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public JsonWebToken setType(String type) {
-        this.type = type;
-        return this;
+        }
     }
 
     /**
@@ -211,22 +109,47 @@ public class JsonWebToken implements Serializable, Token {
      *
      * @return
      */
-    @JsonAnyGetter
     public Map<String, Object> getOtherClaims() {
         return otherClaims;
     }
 
-    @JsonAnySetter
-    public void setOtherClaims(String name, Object value) {
-        otherClaims.put(name, value);
-    }
-
-    @Override
-    public TokenCategory getCategory() {
-        return TokenCategory.INTERNAL;
-    }
-
     private static int getCurrentTimeInSeconds() {
         return ((int) (System.currentTimeMillis() / 1000));
+    }
+
+    private Long getClaimValueAsLong(String claimName) {
+        try {
+            Long claimValue = jwtClaims.getClaimValue(claimName, Long.class);
+            if (claimValue == null) {
+                claimValue = 0L;
+            }
+        } catch (MalformedClaimException e) {
+
+        }
+    }
+
+    protected Object getClaimValue(String claimName) {
+        Object claim = null;
+
+        switch (claimName) {
+            // convert NumericDate values to Long
+            case EXP:
+            case IAT:
+            case AUTH_TIME:
+            case NBF:
+            case UPDATED_AT:
+                try {
+                    claim = jwtClaims.getClaimValue(claimName, Long.class);
+                    if (claim == null) {
+                        claim = 0L;
+                    }
+                } catch (MalformedClaimException e) {
+                    throw log.
+                }
+                break;
+            default:
+                claim = jwtClaims.getClaimValue(claimName);
+        }
+        return claim;
     }
 }
